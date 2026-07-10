2ada7d9983f1141c749273703136cee903b2cc2b
Keerthik <keerthikkdu@gmail.com>
2026-07-10 13:04:50 +0530

    Replace project with SFDX project

---
+++ force-app/main/default/lwc/customTab/customTab.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from "lwc";
+
+export default class CustomTab extends LightningElement {
+    @api previewData;
+}
---
+++ force-app/main/default/uiBundles/CustomerPortal/src/main.jsx
@@ -0,0 +1,3 @@
+import App from "./App";
+
+console.log("Customer Portal Loaded");
